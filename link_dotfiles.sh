080121c6fb175e0bf214f27cfb74ffa281cf5ba1
pascal-riesinger <pascal.riesinger@sap.com>
2018-10-16 17:00:29 +0200

    Update stale files, structuring

--- link_dotfiles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-[ -d "$HOME/.dotfiles" ] && export DOTFILES="$HOME/.dotfiles" || exit 1
-
-ln -s "$DOTFILES/zshrc.symlink" "$HOME/.zshrc"
-ln -s "$DOTFILES/tmux.conf.symlink" "$HOME/.tmux.conf"
-mkdir -p "$HOME/.config/nvim"
-ln -s "$DOTFILES/config/nvim/init.vim" "$HOME/.config/nvim/init.vim"
-
